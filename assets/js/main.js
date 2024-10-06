658cc0f19aa6b35a9da60b5f1e016e0008d39a91
Moahmmed Gomaa <mohammedgomaa2522@gmail.com>
2024-10-06 18:33:20 +0300

    edit form

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,3 +26,28 @@ const navActive = () => {
     })
 };
 navActive();
+// ***************************************************
+
+function sendMail() {
+    var params = {
+        fullName: document.getElementById("fullName").value,
+        email: document.getElementById("email").value,
+        whatsapp: document.getElementById("whatsapp").value,
+        message: document.getElementById("message").value,
+        file: document.getElementById("file").files[0].name,
+    }
+    // -----------
+    const serviceID = "service_3x8kvi7";
+    const templateID = "template_w79ckqf"
+    if (params.fullName != "" && params.email != "" && params.whatsapp != "" && params.message != "") {
+        emailjs.send(serviceID, templateID, params).then((res) => {
+            alert("تم ارسال رسالتك بنجاح سيتم التواصل معك في اقرب وقت");
+            document.getElementById("fullName").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("whatsapp").value = "";
+            document.getElementById("message").value = "";
+        }).catch((err) => console.log(err));
+    } else {
+        alert("يجب ادخال جميع البياناتا")
+    }
+};
